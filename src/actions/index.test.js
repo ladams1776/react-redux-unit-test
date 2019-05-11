b0583fabf3a961606628b3305a4bacbbb5613c4b
larry adams <ladams1776@yahoo.com>
2019-05-11 10:59:04 -0400

    Add the index.test.js to test the action creators

---
+++ src/actions/index.test.js
@@ -0,0 +1,17 @@
+import * as actions from './index';
+import { stack, stacks} from '../data/fixtures';
+
+
+
+describe('actions', () => {
+
+  it('creates an action to set the main stack', () => {
+    const expectedAction = {
+      type: actions.SET_STACK,
+      stack: {...stack}
+    }
+
+    expect(actions.setStack(stack)).toEqual(expectedAction);
+  });
+  
+});
