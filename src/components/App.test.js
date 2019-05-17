46e66fc0c766188927e1f6b8e9af456bbdf9c491
larry adams <ladams1776@yahoo.com>
2019-05-16 21:13:59 -0400

    Create and finish the App.test.js

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -11,4 +11,14 @@ describe("<App />", () => {
   it("renders the title", () => {
     expect(wrapper.find('h2').text()).toEqual('Jeopardy!');
   });
+
+  it('creates the correct number of links', () => {
+    expect(wrapper.find('Link').length).toEqual(categories.length);
+  });
+
+  it('titles the links correctly', () => {
+    wrapper.find('Link h4').forEach((linkTitle, index) => {
+      expect(linkTitle.text()).toEqual(categories[index].title);
+    });
+  });
 });
